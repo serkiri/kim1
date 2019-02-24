fcb4ef50bcdcdec101570b16e35df04c2e01f461
Sergiy Kyrylenko <Sergiy_Kyrylenko@epam.com>
2019-02-24 16:21:35 +0200

    Added decoder. Test as seconds counter

--- kim1_top.vhd
+++ kim1_top.vhd
@@ -19,8 +19,13 @@ architecture behavior of kim1_top is
 	signal vga_hpos		: integer range 0 to 1000;
 	signal vga_vpos		: integer range 0 to 1000;
 	
-	signal segment_draw 	: std_logic_vector(5 downto 0);
+	signal led_output 	: std_logic_vector(5 downto 0);
+	
+	type LedArray is array (0 to 5) of std_logic_vector(6 downto 0);
+	signal ledSegments	: LedArray;
+	signal ledValue		: std_logic_vector (23 downto 0) := x"000000";
 
+	
 	signal oneSecCount 	: integer := 0;
 	signal oneSecond		: std_logic := '0';
 	
@@ -49,7 +54,7 @@ begin
 			vga_vsync => VIDEO_VSYNC
 		);
 
-	segments : for i in 0 to 5 generate	
+	generateLeds : for i in 0 to 5 generate	
 	begin
 		segmentInst : entity work.segment
 			port map (
@@ -58,9 +63,10 @@ begin
 				width		=> SEG_WIDTH,
 				length	=>	SEG_LENGTH,
 				thick		=> SEG_THICK,
+				segments => ledSegments(i),
 				hpos => vga_hpos,
 				vpos => vga_vpos,
-				draw => segment_draw(i)
+				draw => led_output(i)
 			);
 	end generate;
 	
@@ -70,15 +76,16 @@ begin
 			if (oneSecCount >= 20000000) then
 					oneSecCount <= 0;
 					oneSecond <= not(oneSecond);
+					ledValue <= std_logic_vector( unsigned(ledValue) + 1 );
 			else
 				oneSecCount <= oneSecCount + 1;
 			end if;
 		end if;
 	end process;
 
-	draw_segment : process(segment_draw)
+	draw_segment : process(led_output)
 	begin
-		if(segment_draw /= ZEROS)then
+		if(led_output /= ZEROS)then
 			VIDEO_R <= "01111111";
 			VIDEO_G <= "00000000";
 			VIDEO_B <= "00000000";
@@ -89,5 +96,28 @@ begin
 		end if;
 	end process;
 		
+
+	generateLedValues : for i in 0 to 5 generate	
+	begin
+		with ledValue((5-i)*4 + 3 downto (5-i)*4) select ledSegments(i) <=
+			"0111111" when x"0",
+			"0000110" when x"1",
+			"1011011" when x"2",
+			"1001111" when x"3",
+			"1100110" when x"4",
+			"1101101" when x"5",
+			"1111101" when x"6",
+			"0000111" when x"7",
+			"1111111" when x"8",
+			"1101111" when x"9",
+			"1110111" when x"a",
+			"1111100" when x"b",
+			"0111001" when x"c",
+			"1011110" when x"d",
+			"1111001" when x"e",
+			"1110001" when x"f";
+	end generate;
+	
+
 end behavior;
 	
--- segment.vhd
+++ segment.vhd
@@ -9,24 +9,25 @@ entity segment is
 		width		: in integer;
 		length	: in integer;
 		thick		: in integer;
-		hpos	: in integer range 0 to 1000;
-		vpos	: in integer range 0 to 1000;
-		draw	: out std_logic := '0'
+		segments	: in std_logic_vector (6 downto 0);
+		hpos		: in integer range 0 to 1000;
+		vpos		: in integer range 0 to 1000;
+		draw		: out std_logic := '0'
 	);
 end segment;
 
 architecture behavior of segment is
 
 begin
-	drawOut:process(hpos, vpos, width, length, thick, xoffset, yoffset)
+	drawOut:process(hpos, vpos, width, length, thick, xoffset, yoffset, segments)
 	begin
-		if(hpos >= xoffset and hpos < xoffset + width and vpos >= yoffset and vpos < yoffset + thick) or --a
-		(hpos >= xoffset + width - thick and hpos < xoffset + width and vpos >= yoffset and vpos < yoffset + (length + thick)/2) or--b
-		(hpos >= xoffset + width - thick and hpos < xoffset + width and vpos >= yoffset + (length - thick)/2 and vpos < yoffset + length) or  --c
-		(hpos >= xoffset and hpos < xoffset + width and vpos >= yoffset + length - thick and vpos < yoffset + length) or --d
-		(hpos >= xoffset and hpos < xoffset + thick and vpos >= yoffset + (length - thick)/2 and vpos < yoffset + length) or--e
-		(hpos >= xoffset and hpos < xoffset + thick and vpos >= yoffset and vpos < yoffset + (length + thick)/2) or  --f
-		(hpos >= xoffset and hpos < xoffset + width and vpos >= yoffset + (length - thick)/2 and vpos < yoffset + (length + thick)/2) then  --g
+		if(segments(0) = '1' and hpos >= xoffset and hpos < xoffset + width and vpos >= yoffset and vpos < yoffset + thick) or --a
+		(segments(1) = '1' and hpos >= xoffset + width - thick and hpos < xoffset + width and vpos >= yoffset and vpos < yoffset + (length + thick)/2) or--b
+		(segments(2) = '1' and hpos >= xoffset + width - thick and hpos < xoffset + width and vpos >= yoffset + (length - thick)/2 and vpos < yoffset + length) or  --c
+		(segments(3) = '1' and hpos >= xoffset and hpos < xoffset + width and vpos >= yoffset + length - thick and vpos < yoffset + length) or --d
+		(segments(4) = '1' and hpos >= xoffset and hpos < xoffset + thick and vpos >= yoffset + (length - thick)/2 and vpos < yoffset + length) or--e
+		(segments(5) = '1' and hpos >= xoffset and hpos < xoffset + thick and vpos >= yoffset and vpos < yoffset + (length + thick)/2) or  --f
+		(segments(6) = '1' and hpos >= xoffset and hpos < xoffset + width and vpos >= yoffset + (length - thick)/2 and vpos < yoffset + (length + thick)/2) then  --g
 			draw <= '1';
 		else
 			draw <= '0';
